d5e49174ac2dfc4497ced67d3a8141131f1fbbfa
Megablay Xdd <zeroxydev@gmail.com>
2022-05-18 09:04:51 +0200

    Minor Fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -8,6 +9,11 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    
     // ...add more providers here
   ],
   callbacks: {
