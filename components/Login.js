23ea71dc02791b6530882d79f50a881ddcd4266a
Megablay Xdd <zeroxydev@gmail.com>
2022-05-20 01:11:44 +0200

    Old Login Test

--- components/Login.js
+++ components/Login.js
@@ -1,19 +1,9 @@
 import { signIn } from "next-auth/react";
 import Image from "next/image";
-import {
-  HeartIcon as HeartIconFilled,
-  ChatIcon as ChatIconFilled,
-  BadgeCheckIcon as BadgeCheckIconFilled,
-} from "@heroicons/react/solid";
-import '@fortawesome/fontawesome-free/js/fontawesome';
-import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
 
 function Login({ providers }) {
   return (
-    
-    <div className="flex flex-col items-center h-[65vh] sm:h-[75vh] bg-black mt-[15vh] sm:mt-[25vh]">
+    <div className="flex flex-col items-center space-y-20 pt-48">
 {/*       <Image
         src="https://rb.gy/ogau5a"
         width={150}
@@ -21,52 +11,23 @@ function Login({ providers }) {
         objectFit="contain"
       /> */}
 
-
-      <div className="flex flex-row justify-center items-center ">
-          <div className="bg-[#ffffff] rounded-xl flex justify-center items-center w-[320px] h-[430px]">
-            <div className=" text-black absolute -mt-5 text-center">
-              <h1 className=" text-2xl font-medium mb-2">SIGN IN</h1>
-              <form className="text-white text-center mt-[20px]">        
-              <input className="text-center w-[150px] m-auto block my-3 px-2 rounded-3xl bg-black py-3 transition-padding duration-300 ease-in-out hover:w-[237px]" placeholder="Username"></input>
-              <input className="text-center w-[150px] m-auto block my-3 px-2 rounded-3xl bg-black py-3 transition-padding duration-300 ease-in-out hover:w-[237px]" placeholder="Password" type="password"></input>
-              <button className="text-center m-auto mt-5 block py-1 bg-black w-[120px] text-white rounded-md transition-padding duration-300 ease-in-out hover:w-[180px]">SIGN IN</button>
-              <p className="text-black block text-xs mt-4">You dont have an account? - <a href="register"><u className="inline-block font-semibold  ">Create Account</u></a></p>
-              <p className="inline-block text-black font-bold max-m-[30px] ml-[88px] mt-[17px] absolute text-center">OR</p>
-              <div className="h-[3px] w-[80px] mr-10 bg-black mx-auto inline-block mt-7"></div>
-              <div className="h-[3px] w-[80px] bg-black inline-block mx-auto mt-7"></div>
-              <div className="flex flex-col justify-center mt-3 -mb-2">
-              <button
-              className="px-[18px] py-[8px] bg-black rounded group mt-[10px] transition-color duration-300 ease-in-out hover:bg-[#242424]"
-              onClick={() => signIn('google')}
-            >
-              <span className=" relative w-full text-left text-white group-hover:text-white">
-                Sign in with Google <p className="inline-block ml-2"><i class="fa-brands fa-google"></i></p>
-              </span>
-            </button>
+      <div>
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            {/* https://devdojo.com/tailwindcss/buttons#_ */}
             <button
-              className="px-[18px] py-[8px] bg-black rounded group mt-[10px] transition-color duration-300 ease-in-out hover:bg-[#242424]"
-              onClick={() => signIn('github')}
+              className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
             >
-              <span className=" relative w-full text-left text-white transition-colors duration-300 ease-in-out group-hover:text-white">
-                Sign in with GitHub <p className="inline-block ml-2"><i class="fa-brands fa-github"></i></p>
+              <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#1d9bf0] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+              <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
+                Sign in with {provider.name}
               </span>
             </button>
-         </div>
-              </form>
-
-            </div>
-
           </div>
-                
+        ))}
       </div>
-
-      <footer className="w-[100%] h-[40px] bg-[#fff0] absolute bottom-0 flex justify-center items-center ">
-      <p className="font-bold text-sm text-center text-white -mt-10"><a target="_blank" href="https://twitter.com/thezeroxy"><p>Coded with<HeartIconFilled className="h-5 inline-block ml-1 mb-1 transition-colors ease-out duration-400 hover:text-[#f34d42]" /> by Zeroxy</p></a></p>
-      </footer>
-
-
     </div>
-    
   );
 }
 
